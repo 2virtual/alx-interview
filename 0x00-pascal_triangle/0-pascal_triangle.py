bd57864551d577fa3353bc16009fd6c2eb4828cf
2virtual <8sinss@gmail.com>
2023-07-06 22:03:35 +0100

    pascal triangle modified

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 from typing import List
 
-
-def pascal_triangle(n: int) -> List[list]:
+def pascal_triangle(n: int) -> List[List[int]]:
     '''
-    Pascal triangle
+    Generate Pascal's triangle up to the given number of all rows
+
+    Args:
+        n (int): Number of rows to generate in Pascal's triangle
+
+    Returns:
+        List[List[int]]: Pascal's triangle represented as a list of lists of integers
     '''
     if n <= 0:
         return []
@@ -19,7 +24,7 @@ def pascal_triangle(n: int) -> List[list]:
 
     for i in range(2, n):
         temp = [1, 1]
-        for j in range(0, len(triangle[-1])-1):
+        for j in range(0, len(triangle[-1]) - 1):
             a = triangle[-1][j]
             b = triangle[-1][j+1]
             temp.insert(-1, a + b)
